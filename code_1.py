ab16334f1befd836d8588db1accbae22f6af49c8
traxylund <143898484+traxylund@users.noreply.github.com>
2023-11-12 06:41:16 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,38 @@
+
+def generate_random_data():
+
+    data = generate_random_data()
+
+    main()
+if __name__ == "__main__":
+    for item in data:
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+import random
+
+def main():    for item in data:
+        print(f"Random Number: {item}")
+    main()
+def generate_random_data():
+def main():
+
+import random
+
+if __name__ == "__main__":
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+        }
+public class RandomDataGenerator {
+        int[] data = new int[10];
+
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+    }
+        }
